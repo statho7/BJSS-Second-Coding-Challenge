d02eda9722a478096672309225284fc9dae5d8ab
statho7 <andreas.stathop@gmail.com>
2021-08-18 23:53:27 +0100

    Made code simpler

--- main.py
+++ main.py
@@ -1,47 +1,40 @@
 def LBBT_tax(property_value):
     '''
-    Up to £145,000          0%
+    This function calculates tax based on the value of the property.
+    
+    Args:
+        property_value: The value of the property we want to calculate the LBBT tax.
 
-    £145,001 to £250,000    2%
-
-    £250,001 to £325,000    5%
-
-    £325,001 to £750,000    10%
-
-    Over £750,000           12%
+    Returns: The calculated tax based on the value of the property.
     '''
-    # '''
+
+    # Up to £145,000          0%
+
+    # £145,001 to £250,000    2%
+
+    # £250,001 to £325,000    5%
+
+    # £325,001 to £750,000    10%
+
+    # Over £750,000           12%
+
     taxes = {   '1st band':{'lower':0,'upper':145000,'rate':0},
                 '2nd band':{'lower':145000,'upper':250000,'rate':2},
                 '3rd band':{'lower':250000,'upper':325000,'rate':5},
                 '4th band':{'lower':325000,'upper':750000,'rate':10},
-                '5th band':{'lower':750000,'upper':1000000000,'rate':12}
+                '5th band':{'lower':750000,'upper':10000000000,'rate':12}
             }
-    # '''
-    # print(taxes['2nd band']['lower'], taxes['2nd band']['upper'], taxes['2nd band']['rate'])
+            
     try:
         if property_value:
-            if property_value <= 145000:
-                return 0
-            else:
-                tax = 0
-
-                if property_value > 250000:
-                    tax += (250000 - 145000) * 0.02
+            tax = 0
+            for key, value in taxes.items():
+                if property_value > value['upper']:
+                    tax += (value['upper'] - value['lower']) * value['rate'] / 100
                 else:
-                    return (property_value - 145000) * 0.02
-
-                if property_value > 325000:
-                    tax += (325000 - 250000) * 0.05
-                else:
-                    return tax + (property_value - 250000) * 0.05
-
-                if property_value > 750000:
-                    tax += (750000 - 325000) * 0.10 + (property_value - 750000) * 0.12
-                else:
-                    return tax + (property_value - 325000) * 0.10
-                    
-                return tax
+                    tax += (property_value - value['lower']) * value['rate'] / 100
+                    break
+            return tax
         else:
             return 'please enter number'
     except TypeError as err:
